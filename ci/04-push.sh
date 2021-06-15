a57e8750b3ace2623ce5b3e57b86cfa8a155d853
Ljupco Kostic <8545397+LjupcoKostic@users.noreply.github.com>
2021-06-15 11:41:42 +0200

    Update 04-push.sh

--- ci/04-push.sh
+++ ci/04-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker image build -t sixeyed/pi-psod -f ./docker/web/Dockerfile .
+docker image build -t ljupchokostic/jenkins:latest -f ./docker/web/Dockerfile .
 
-if [ -z ${DOCKER_HUB_USER+x} ]
-then 
-    echo 'Skipping login - credentials not set' 
-else 
-    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-fi
+#if [ -z ${DOCKER_HUB_USER+x} ]
+#then 
+#    echo 'Skipping login - credentials not set' 
+#else 
+#    docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+#fi
 
-docker image push sixeyed/pi-psod
+docker image push ljupchokostic/jenkins:latest
